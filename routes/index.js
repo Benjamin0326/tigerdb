9eb5fdad3f35168d15ef7b7eacfaf3acff33af9b
benjamin <benjamin0326@naver.com>
2016-05-09 13:56:58 +0900

    test version

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,32 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/home', function(req, res, next){
-  res.render('home');
+  var progress_values = [{name:"test1", value:30}, {name:"test2", value:44}, {name:"test3", value:100}, {name:"test4", value:73}];
+  var notice_values = ["title1", "title2", "title3"];
+  res.render('home', {progress_values:progress_values, notice_values:notice_values});
+});
+
+router.get('/signup', function(req, res, next){
+  res.render('signup');
+});
+
+router.get('/phone', function(req, res, next){
+  res.render('phone');
+});
+
+router.get('/profile', function(req, res, next){
+  res.render('profile');
+});
+
+router.get('/test', function(req, res, next){
+  res.render('test');
+});
+
+router.get('/notice', function(req, res, next){
+  var notice_values=[{title:"title1", content:"It's title1's content (content1)"},
+                    {title:"title2", content:"It's title2's content (content2)"},
+                    {title:"title3", content:"It's title1's content (content3)"}];
+  res.render('notice', {notice_values:notice_values});
 });
 
 module.exports = router;
