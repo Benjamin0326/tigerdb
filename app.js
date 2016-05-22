c45f6cf2deb35515cd56bf3a4dc76868ff8a9dd0
kyung-joo <rudwn826@gmail.com>
2016-05-22 21:48:11 +0900

    usim

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 var notice = require('./routes/notice');
 var phone = require('./routes/phone');
+var usim = require('./routes/usim');
 var people = require('./routes/people');
 var schedule = require('./routes/schedule');
 var bug = require('./routes/bug');
@@ -41,6 +42,7 @@ app.use('/phone', phone);
 app.use('/schedule', schedule);
 app.use('/people', people);
 app.use('/bug', bug);
+app.use('/usim', usim);
 //app.use('/notices', notices);
 
 
---
+++ routes/usim.js
@@ -0,0 +1,131 @@
+var express = require('express');
+var oracledb = require('oracledb');
+var router = express.Router();
+
+router.get('/', function(req, res, next){
+  console.log('here');
+  res.render('usim,', {emp:req.session});
+  //oracledb.maxRows=100;
+  /*
+  oracledb.getConnection(
+    {
+      user          : "SYSTEM",
+      password      : "0305",
+      connectString : "localhost/DBSERVER"
+    },
+    function(err, connection)
+    {
+      if (err) { console.error(err.message); return; }
+      connection.execute(
+        "SELECT * from usim",  // bind value for :id
+        function(err, result)
+        {
+          if (err) { console.error(err.message); return; }
+          console.log(result.rows);
+          res.render('usim,', {emp:req.session, usims:result.rows});
+        });
+    });
+*/
+});
+/*
+router.get('/usim_add', function(req, res){
+  console.log('here');
+  res.render('hardware/usim_add', {emp:req.session});
+})
+
+router.post('/add_commit', function(req, res, next){
+  
+  var data = JSON.parse(req.body.data); 
+  console.log(data)
+  
+  var phonename = data[0].phonename;
+  var manu = data[0].manufacture;
+  var osver = data[0].osver;
+  
+  oracledb.getConnection(
+    {
+      user          : "SYSTEM",
+      password      : "0305",
+      connectString : "localhost/DBSERVER"
+    },
+    function(err, connection)
+    {
+      if (err) { console.error(err.message); return; }
+
+      connection.execute(
+        "insert into phone (PHONENAME, MANUFACTURE, OSVER, STATE) VALUES('"+phonename+"', '"+manu+"', '"+osver+"', 0)",
+        function(err, result)
+        {
+          if (err) { console.error(err.message); return; }
+          connection.commit(function(err){
+            if(err){
+              res.send("실패했습니다.");
+              return;
+            }
+          });
+          res.redirect('/phone');
+        });
+    });
+});
+
+router.post('/commit', function(req, res, next){
+  console.log("test phone");
+  var editdata = JSON.parse(req.body.data);
+  console.log(editdata);
+  var i = 0;
+  var len = editdata.length;
+  console.log("length : " + len);
+    oracledb.getConnection(
+    {
+      user          : "SYSTEM",
+      password      : "0305",
+      connectString : "localhost/DBSERVER"
+    },
+    function(err, connection)
+    {
+      if (err) { console.error(err.message); return; }
+      for(i=0; i<len; i++){
+        var t_id = editdata[i].id;
+        var t_phonename = editdata[i].phonename;
+        var t_manu = editdata[i].manufacture;
+        var t_os = editdata[i].osver;
+        var t_state = editdata[i].state;
+        var t_renter = editdata[i].renter;
+
+        if(t_id!=null){
+          connection.execute(
+            "UPDATE phone set phonename='"+t_phonename+"', manufacture='"+t_manu+"', osver='"+t_os+"', state='"+t_state+"', renter='"+t_renter+"' where phoneno='"+t_id+"'",  // bind value for :id
+            function(err, result)
+            {
+              if (err) { console.error(err.message); return; }
+              connection.commit(function(err){
+                if(err){
+                  res.send("실패했습니다.");
+                  return;
+                }
+              });
+            });
+        }
+        else{
+          console.log("it'will be delete part");
+          t_id=editdata[i];
+          console.log("delete : "+t_id);
+          connection.execute(
+            "delete from phone where phoneno='"+t_id+"'",  // bind value for :id
+            function(err, result)
+            {
+              if (err) { console.error(err.message); return; }
+              connection.commit(function(err){
+                if(err){
+                  res.send("실패했습니다.");
+                  return;
+                }
+              });
+            });
+        }
+      }
+
+    });
+});
+*/
+module.exports = router;
